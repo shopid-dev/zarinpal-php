e94b1e68cf1e1292be22f81de132d5da37f663e5
behnam <vladimirbooithsek@gmail.com>
2022-04-02 21:33:58 +0430

    init

---
+++ readme.md
@@ -0,0 +1,76 @@
+# zarinpal php package
+
+    <?php
+    
+      
+    
+    //make zarinpal object
+    
+    $zarinpal = new zarinPal([
+    
+    "callBackUrl" => "https://www.mywebsice.com/zarinpalverify",
+    
+    "merchantId" => "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
+    
+    ]);
+    
+      
+    
+    //make a request
+    
+      
+    
+    try {
+    
+    $request = $zarinpal->apiRequest([
+    
+    "amount" => "1000",
+    
+    "description" => "payment #999",
+    
+    "email" => "me@web.net",
+    
+    "mobile" => "09120000000",
+    
+    ]);
+    
+    var_dump(json_decode($request));
+    
+    } catch (Exception $error) {
+    
+    var_dump(json_decode($error->getMessage()));
+    
+    }
+    
+      
+      
+    
+    //verify peyment
+    
+    try {
+    
+    $verify = $zarinpal->verify(
+    
+    [
+    
+    "authority" => "A00000000000000000000000000000000000",
+    
+    "amount" => "1000"
+    
+    ]
+    
+    );
+    
+    var_dump(json_decode($verify));
+    
+    } catch (Exception $error) {
+    
+    var_dump(json_decode($error->getMessage()));
+    
+    }
+    
+      
+    
+    ?>
+
+
---
+++ src/zarinPal.php
@@ -0,0 +1,103 @@
+<?php
+/*
+        https://www.shopd.ir
+        https://github.com/shopid-dev
+        alaeebehnam@gmail.com
+        https://t.me/theycallmebehnam
+
+*/
+namespace shopid;
+
+
+
+class zarinPal
+{
+    private  $callBackUrl;
+    private  $merchantId;
+    function __construct($param)
+    {
+
+        $this->callBackUrl = $param['callBackUrl'];
+        $this->merchantId = $param['merchantId'];
+    }
+
+    public function apiRequest($param)
+    {
+
+        $data = array(
+            "merchant_id" => $this->merchantId,
+            "amount" => $param['amount'],
+            "callback_url" => $this->callBackUrl,
+            "description" => $param['description'],
+            "metadata" => ["email" => $param['email'], "mobile" => $param['mobile']],
+        );
+
+        $jsonData = json_encode($data);
+        $ch = curl_init('https://api.zarinpal.com/pg/v4/payment/request.json');
+        curl_setopt($ch, CURLOPT_USERAGENT, 'ZarinPal Rest Api v1');
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($jsonData)
+        ));
+
+        $result = curl_exec($ch);
+        $err = curl_error($ch);
+        $result = json_decode($result, true, JSON_PRETTY_PRINT);
+
+
+        curl_close($ch);
+
+        if ($err) {
+            throw new \Exception(json_encode(["error" => "cURL Error #:" . $err]));
+        } else {
+            if (empty($result['errors'])) {
+                if ($result['data']['code'] == 100) {
+                    return json_encode(["url" => 'https://www.zarinpal.com/pg/StartPay/' . $result['data']["authority"], "authority" => $result['data']["authority"]]);
+                }
+            } else {
+
+                throw new \Exception(json_encode(["error" => $result['errors']]));
+            }
+        }
+    }
+
+
+    public function verify($param)
+    {
+
+        $data = array("merchant_id" => $this->merchantId, "authority" => $param['authority'], "amount" => $param['amount']);
+        $jsonData = json_encode($data);
+        $ch = curl_init('https://api.zarinpal.com/pg/v4/payment/verify.json');
+        curl_setopt($ch, CURLOPT_USERAGENT, 'ZarinPal Rest Api v4');
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($jsonData)
+        ));
+
+        $result = curl_exec($ch);
+        $err = curl_error($ch);
+        curl_close($ch);
+        $result = json_decode($result, true);
+
+        if ($err) {
+            throw new \Exception(json_encode(["error" => "cURL Error #:" . $err]));
+        } else {
+            if (isset($result['errors']) && count($result['errors']) > 0) {
+                throw new \Exception(json_encode(["error" => $result['errors']]));
+            } else {
+
+                return json_encode($result['data']);
+            }
+        }
+    }
+}
+
+
+
+
